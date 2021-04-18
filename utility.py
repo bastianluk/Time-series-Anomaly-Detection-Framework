928ab5a554601dbcbe9b4b8c84399e5fb8264f22
bastianluk <luk3bastian@gmail.com>
2021-04-18 12:08:10 +0200

    whitespaces

--- utility.py
+++ utility.py
@@ -7,16 +7,16 @@ def add_day_column(row):
     locale.setlocale(locale.LC_ALL,'en_US.UTF-8')
     return d.datetime.strptime(row['time'], '%d.%m.%Y %H:%M').date().strftime("%A").lower()
 
-def add_interval_column(row): 
+def add_interval_column(row):
     return str(row['time']).split(" ")[1]
 
-def add_hour_column(row): 
+def add_hour_column(row):
     return int(str(row['interval']).split(":")[0])
 
-def add_minute_column(row): 
+def add_minute_column(row):
     return str(row['interval']).split(":")[1]
 
-def add_date_column(row): 
+def add_date_column(row):
     return str(row['time']).split(" ")[0]
 
 def add_day_of_month_column(row):
@@ -24,5 +24,3 @@ def add_day_of_month_column(row):
 
 def add_month_index_column(row):
     return str(row['date']).split(".")[1]
-
-
